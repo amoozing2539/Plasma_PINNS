dc1a030b396b9bc978bb4eb5471bbd0f8a027ce3
aleMotzz <aleotz@g.ucla.edu>
2025-03-03 02:12:05 -0800

    Update alfven_data_visualized.py

---
+++ alfven_data_visualized.py
@@ -0,0 +1,18 @@
+import h5py 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+##read the hd5 file####
+
+
+###Visualize the scalar field ### (Electirc potential)
+
+##
+
+
+
+
+if __name__ == '__main__':
+#   main()
